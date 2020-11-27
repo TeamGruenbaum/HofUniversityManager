8bb6e241a47b4e76f2c02525d5222e045252a0bb
patdre <p.dressel99@gmail.com>
2020-11-27 16:01:44 +0100

    Ergänzungen bzgl. Menü-Button

--- StudiPlaner/Controller.java
+++ StudiPlaner/Controller.java
@@ -5,7 +5,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,11 +14,23 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        editMenuButton();
+        setTheme(true);
+    }
+
+    public void editMenuButton() {
+        // Menu-Button with Image
         Image openIcon = new Image(getClass().getResourceAsStream("img/dots-menu.png"));
         ImageView view = new ImageView(openIcon);
         view.setFitHeight(30);
         view.setPreserveRatio(true);
 
         mb.setGraphic(view);
+    }
+
+    public void setTheme(Boolean themeStatus) {
+        if(themeStatus==true) {
+            // Edit elements for changing color while using dark mode
+        }
     }
 }
