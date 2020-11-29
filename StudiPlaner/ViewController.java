4bdeaa3d68543c39e704d8436e331a063aa97ebf
patdre <p.dressel99@gmail.com>
2020-11-29 11:57:58 +0100

    Umbenennung Controller

--- StudiPlaner/ViewController.java
+++ StudiPlaner/ViewController.java
@@ -8,7 +8,7 @@ import javafx.scene.image.ImageView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class ViewController implements Initializable {
     @FXML
     private MenuItem mb;
 
