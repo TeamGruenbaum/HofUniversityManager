83304fb4c18c3e6460148d5a38a11cc2fbd375ae
stevensolleder <stevensolleder@posteo.de>
2021-01-18 21:40:06 +0100

    Fix

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -27,16 +27,16 @@ import javax.imageio.ImageIO;
 
 public class Main extends Application
 {
-	private static Parent root;
-	private static HostServices hostServices;
+	private static Parent rootParent;
+	private static HostServices programmOeffnerHostServices;
 	private static Stage primaryStage;
 
 	@Override public void start(Stage primaryStage) throws Exception
 	{
-        /*if(System.getProperty("os.name").contentEquals("Mac OS X"))
+        if(System.getProperty("os.name").contentEquals("Mac OS X"))
         {
             Taskbar.getTaskbar().setIconImage(ImageIO.read(getClass().getResource("Grafiken/HUM_Icon_Short.png")));
-        }*/
+        }
 
 		if(SchreiberLeser.isErsterStart())
 		{
@@ -47,11 +47,11 @@ public class Main extends Application
 			}
 			else
 			{
-				Alert alert=new Alert(Alert.AlertType.WARNING);
-				alert.setHeaderText("Internetverbindung notwendig");
-				alert.setContentText("Beim ersten Start des Programmes ist es zwingend notwendig, dass Du eine Verbindung zum Internet hast. Starte das Programm erneut, wenn Du eine Verbindung zum Internet hergestellt hast.");
-				alert.getDialogPane().getStylesheets().add(getClass().getResource("View/CSS/Application.css").toExternalForm());
-				alert.showAndWait().ifPresentOrElse((item)->
+				Alert keinInternetAlert=new Alert(Alert.AlertType.WARNING);
+				keinInternetAlert.setHeaderText("Internetverbindung notwendig");
+				keinInternetAlert.setContentText("Beim ersten Start des Programmes ist es zwingend notwendig, dass Du eine Verbindung zum Internet hast. Starte das Programm erneut, wenn Du eine Verbindung zum Internet hergestellt hast.");
+				keinInternetAlert.getDialogPane().getStylesheets().add(getClass().getResource("View/CSS/Application.css").toExternalForm());
+				keinInternetAlert.showAndWait().ifPresentOrElse((item)->
 				{
 					Platform.exit();
 					System.exit(0);
@@ -77,11 +77,11 @@ public class Main extends Application
 			{
 				keineGefahrException.printStackTrace();
 
-				Alert alert=new Alert(Alert.AlertType.ERROR);
-				alert.setHeaderText("Daten korrupt");
-				alert.setContentText("Die gespeicherten Daten wurden manipuliert. Deshalb wir das Programm nach dem Schließen des Fensters zurückgesetzt.");
-				alert.getDialogPane().getStylesheets().add(getClass().getResource("/View/CSS/Application.css").toExternalForm());
-				alert.showAndWait().ifPresentOrElse((item)->
+				Alert datenKorruptAlert=new Alert(Alert.AlertType.ERROR);
+				datenKorruptAlert.setHeaderText("Daten korrupt");
+				datenKorruptAlert.setContentText("Die gespeicherten Daten wurden manipuliert. Deshalb wir das Programm nach dem Schließen des Fensters zurückgesetzt.");
+				datenKorruptAlert.getDialogPane().getStylesheets().add(getClass().getResource("/View/CSS/Application.css").toExternalForm());
+				datenKorruptAlert.showAndWait().ifPresentOrElse((item)->
 				{
 					SchreiberLeser.alleDatenLoeschen();
 					Platform.exit();
@@ -95,39 +95,30 @@ public class Main extends Application
 			}
 		}
 
-		hostServices=getHostServices();
+		programmOeffnerHostServices=getHostServices();
 
-		root=FXMLLoader.load(getClass().getResource("/View/GrundView.fxml"));
+		rootParent=FXMLLoader.load(getClass().getResource("/View/GrundView.fxml"));
 		primaryStage.setTitle("HofUniversityManager");
-		primaryStage.setScene(new Scene(root, 900, 600));
-		root.getStylesheets().add(getClass().getResource("/View/CSS/Application.css").toExternalForm());
+		primaryStage.setScene(new Scene(rootParent, 900, 600));
+		rootParent.getStylesheets().add(getClass().getResource("/View/CSS/Application.css").toExternalForm());
 		Font.loadFont(Main.class.getResource("/Schriften/Rubik-Regular.ttf").toExternalForm(), 10);
 		primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/Grafiken/HUM_Icon_Short.png")));
-
-        /*final URL imageResource = Main.class.getClassLoader().getResource("../Ressourcen/Grafiken/HUM_Icon_Short.png");
-        final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
-        final java.awt.Image image = defaultToolkit.getImage(imageResource);
-        final Taskbar taskbar = Taskbar.getTaskbar();
-        taskbar.setIconImage(image);*/
 
 		GrundViewController.setThema(SchreiberLeser.getNutzerdaten().getAktuellesThema());
 
 		primaryStage.setMinHeight(600);
 		primaryStage.setMinWidth(900);
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>()
+		primaryStage.setOnCloseRequest(t->
 		{
-			@Override public void handle(WindowEvent t)
+			SchreiberLeser.alleSpeichern();
+
+			if(SchreiberLeser.isErsterStart())
 			{
-				SchreiberLeser.alleSpeichern();
-
-				if(SchreiberLeser.isErsterStart())
-				{
-					SchreiberLeser.isErsterStartSetzen();
-				}
-
-				Platform.exit();
-				System.exit(0);
+				SchreiberLeser.isErsterStartSetzen();
 			}
+
+			Platform.exit();
+			System.exit(0);
 		});
 		Main.primaryStage=primaryStage;
 		primaryStage.show();
@@ -140,12 +131,12 @@ public class Main extends Application
 
 	public static Parent getRoot()
 	{
-		return root;
+		return rootParent;
 	}
 
 	public static void oeffneLinkInBrowser(String url)
 	{
-		hostServices.showDocument(url);
+		programmOeffnerHostServices.showDocument(url);
 	}
 
 	public static void main(String[] args)
